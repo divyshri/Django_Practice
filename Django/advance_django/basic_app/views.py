7fb5813f79c89d36e3ad8efe8a7243c245e9febb
divyshri <divyanshu4shrivastava@gmail.com>
2020-06-16 12:56:53 +0530

    CreateView UpdateView DeleteView Practice

--- Django/advance_django/basic_app/views.py
+++ Django/advance_django/basic_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import View,TemplateView,ListView,DetailView
+from django.views.generic import View,TemplateView,ListView,DetailView,CreateView,UpdateView,DeleteView
 from . import models
+from django.urls import reverse_lazy
 
 # Create your views here.
 class SchoolListView(ListView):
@@ -12,7 +13,17 @@ class SchoolDetailView(DetailView):
     model = models.School
     template_name = 'basic_app/school_detail.html'
 
+class SchoolCreateView(CreateView):
+    fields = ('name','principal','location')
+    model = models.School
 
+class SchoolUpdateView(UpdateView):
+    fields = ('name','principal')
+    model = models.School
+
+class SchoolDeleteView(DeleteView):
+    model = models.School
+    success_url = reverse_lazy("basic_app:list")
 
 class IndexView(TemplateView):
     template_name = 'index.html'
